d4edf8f5cd44827c1d9f6c5b624c519a01f487df
ospoon <zxin088@gmail.com>
2019-01-20 23:31:12 +0800

    添加聊天宝添加好友，发送固定图片聊天，固定刷新闻

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def adb_click(x, y):
     command = 'shell input tap ' + str(x) + ' ' + str(y)
     print('点击：' + command)
     print(adb.run(command))
-    time.sleep(random.uniform(1, 5))
+    time.sleep(random.uniform(1, 2))
+
+
+def adb_swipe(x1, y1, x2 ,y2):
+    command = 'shell input swipe ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2)
+    print('滑动：' + command)
+    print(adb.run(command))
+    time.sleep(random.uniform(1, 2))
 
 
 def adb_input(text):
@@ -33,6 +40,22 @@ def adb_key(key):
 
 def analysis(obj):
     if isinstance(obj, dict):
+
+        if obj['@text'] == '添加为熟朋友':
+            x1 = float(obj['@bounds'].split('][')[0][1:].split(',')[0])
+            y1 = float(obj['@bounds'].split('][')[0][1:].split(',')[1])
+            x2 = float(obj['@bounds'].split('][')[1][:-1].split(',')[0])
+            y2 = float(obj['@bounds'].split('][')[1][:-1].split(',')[1])
+
+            print('添加为熟朋友')
+            adb_click(int(random.uniform(x1, x2)), int(random.uniform(y1, y2)))
+
+            print('确认')
+            adb_click(int(random.uniform(558, 966)), int(random.uniform(757, 889)))
+
+            print('返回')
+            adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
+
 
         if obj['@text'] == '添加到通讯录':
 
@@ -80,6 +103,44 @@ def analysis(obj):
             analysis(item)
 
 
+def add_wechar():
+    list_phone = ['qq1825203636', 'qq1825203637', '15545825361']
+
+    for item in list_phone:
+
+        phone = item
+        print('添加好友：' + phone)
+
+        # 定焦输入框
+        print('定焦输入框')
+        adb_click(int(random.uniform(50, 1035)), int(random.uniform(280, 370)))
+
+        # 填写微信号
+        print('填写微信号')
+        adb_input(phone)
+
+        # 搜索好友
+        print('搜索好友')
+        adb_click(int(random.uniform(55, 190)), int(random.uniform(240, 380)))
+
+        # 获取布局
+        adb_dump()
+
+        with open('window_dump.xml', 'r', encoding='utf-8') as rf:
+            window_dump = rf.read()
+            if '添加到通讯录' in window_dump:
+                node_obj = xmltodict.parse(window_dump)['hierarchy']['node']
+                analysis(node_obj)
+            elif '发消息' in window_dump:
+                print('已经是好友了，返回上一页')
+                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                print('返回上一页')
+                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+            elif '该用户不存在' in window_dump:
+                print('用户不存在，返回上一页')
+                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+
+
 if sys.version_info.major != 3:
     print('请使用Python3')
     exit(1)
@@ -96,39 +157,76 @@ VERSION = "0.0.1"
 
 adb.test_device()
 
-list_phone = ['qq1825203636','qq1825203637','15545825361']
 
-for item in list_phone:
-
-    phone = item
-    print('添加好友：' + phone)
-
-    # 定焦输入框
-    print('定焦输入框')
-    adb_click(int(random.uniform(50, 1035)), int(random.uniform(280, 370)))
-
-    # 填写微信号
-    print('填写微信号')
-    adb_input(phone)
-
-    # 搜索好友
-    print('搜索好友')
-    adb_click(int(random.uniform(55, 190)), int(random.uniform(240, 380)))
-
+def add_ltb():
+    adb_swipe(30, 749, 30, 417)
     # 获取布局
     adb_dump()
-
     with open('window_dump.xml', 'r', encoding='utf-8') as rf:
         window_dump = rf.read()
-        if '添加到通讯录' in window_dump:
+        if '添加为熟朋友' in window_dump:
             node_obj = xmltodict.parse(window_dump)['hierarchy']['node']
             analysis(node_obj)
-        elif '发消息' in window_dump:
+        elif '发送消息' in window_dump:
             print('已经是好友了，返回上一页')
-            adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
-            print('返回上一页')
-            adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
-        elif '该用户不存在' in window_dump:
-            print('用户不存在，返回上一页')
-            adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+            adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
 
+
+def add_ltb_main():
+    count = 480 / 5
+
+    print(int(count))
+
+    for item in range(int(count)):
+        adb_click(int(random.uniform(30, 234)), int(random.uniform(417, 649)))
+
+        add_ltb()
+
+        adb_click(int(random.uniform(234, 438)), int(random.uniform(417, 649)))
+
+        add_ltb()
+
+        adb_click(int(random.uniform(438, 642)), int(random.uniform(417, 649)))
+
+        add_ltb()
+
+        adb_click(int(random.uniform(642, 846)), int(random.uniform(417, 649)))
+
+        add_ltb()
+
+        adb_click(int(random.uniform(846, 1050)), int(random.uniform(417, 649)))
+
+        add_ltb()
+
+        adb_swipe(30, 649, 30, 417)
+
+
+def news():
+    while 1:
+
+        # adb_swipe(24, 375, 24, 1047)
+        adb_swipe(24, 1047, 24, 175)
+
+        adb_click(int(random.uniform(24, 1056)), int(random.uniform(375, 475)))# 1047
+
+        adb_swipe(24, 1047, 24, 175)
+        adb_swipe(24, 1047, 24, 175)
+        adb_swipe(24, 1047, 24, 175)
+        adb_swipe(24, 1047, 24, 175)
+        adb_swipe(24, 1047, 24, 175)
+
+        adb_dump()
+        with open('window_dump.xml', 'r', encoding='utf-8') as rf:
+            window_dump = rf.read()
+            if 'com.bullet.messenger:id/news_share_outside_btn' in window_dump:
+                adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
+
+
+def while_send():
+    while 1:
+        adb_click(int(random.uniform(838, 1030)), int(random.uniform(1155, 1347)))
+
+
+if __name__ == '__main__':
+    # add_ltb_main()
+    while_send()
