ba6be8274fbd73501add547eba676f754ea70a80
1825203636@qq.com <1825203636@qq.com>
2019-01-23 18:46:12 +0800

    修改代码调用

--- common/auto_adb.py
+++ common/auto_adb.py
@@ -1,46 +1,50 @@
 # -*- coding: utf-8 -*-
 import os
+import random
 import subprocess
 import platform
+
+import sys
+
+import time
 
 
 class auto_adb():
 
     def __init__(self):
         try:
+            raise OSError('testError')
             adb_path = 'adb'
-            subprocess.Popen([adb_path], stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
+            subprocess.Popen([adb_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             self.adb_path = adb_path
         except OSError:
             if platform.system() == 'Windows':
-                adb_path = os.path.join('Tools', "adb", 'adb.exe')
+                adb_path = os.path.join(os.getcwd(), 'Tools', 'adb.exe')
                 try:
-                    subprocess.Popen(
-                        [adb_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    subprocess.Popen([adb_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     self.adb_path = adb_path
                 except OSError:
                     pass
             else:
                 try:
-                    subprocess.Popen(
-                        [adb_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    subprocess.Popen([adb_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 except OSError:
-                    pass
-            print('请安装 ADB 及驱动并配置环境变量')
-            print('具体链接: https://github.com/wangshub/wechat_jump_game/wiki')
-            exit(1)
+                    print('请安装 ADB 及驱动并配置环境变量')
+                    exit(1)
+
 
     def get_screen(self):
         process = os.popen(self.adb_path + ' shell wm size')
         output = process.read()
         return output
 
+
     def run(self, raw_command):
         command = '{} {}'.format(self.adb_path, raw_command)
         process = os.popen(command)
         output = process.read()
         return output
+
 
     def test_device(self):
         print('检查设备是否连接...')
@@ -58,6 +62,7 @@ class auto_adb():
         for each in output:
             print(each.decode('utf8'))
 
+
     def test_density(self):
         process = os.popen(self.adb_path + ' shell wm density')
         output = process.read()
@@ -68,10 +73,66 @@ class auto_adb():
         output = process.read()
         return output
 
+
     def test_device_os(self):
         process = os.popen(self.adb_path + ' shell getprop ro.build.version.release')
         output = process.read()
         return output
 
-    def adb_path(self):
-        return self.adb_path
+    def test_adb_path(self):
+        return 'ADB PATH : ' + self.adb_path
+
+class adb_tools():
+
+    def __init__(self):
+        if sys.version_info.major != 3:
+            print('请使用Python3')
+            exit(1)
+        try:
+            from common.auto_adb import auto_adb
+        except Exception as ex:
+            print(ex)
+            print('请将脚本放在项目根目录中运行')
+            print('请检查项目根目录中的 common 文件夹是否存在')
+            exit(1)
+        self._adb = auto_adb()
+
+    def print(self):
+        print(self._adb.test_adb_path())
+        print(self._adb.test_device_os())
+        print(self._adb.test_device_detail())
+        print(self._adb.test_density())
+        print(self._adb.get_screen())
+
+        self._adb.test_device()
+
+    def adb_run(self, command):
+        return self._adb.run(command)
+
+    def adb_click(self, x, y):
+        command = 'shell input tap ' + str(x) + ' ' + str(y)
+        print('点击：' + command)
+        print(self._adb.run(command))
+        time.sleep(random.uniform(1, 2))
+
+    def adb_swipe(self, x1, y1, x2, y2):
+        command = 'shell input swipe ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2)
+        print('滑动：' + command)
+        print(self._adb.run(command))
+        time.sleep(random.uniform(1, 2))
+
+    def adb_input(self, text):
+        command = 'shell input text ' + text
+        print('输入：' + command)
+        print(self._adb.run(command))
+        time.sleep(random.uniform(1, 5))
+
+    def adb_dump(self):
+        print('获取布局')
+        print(self._adb.run('shell uiautomator dump'))
+        print(self._adb.run('pull /sdcard/window_dump.xml'))
+
+    def adb_key(self, key):
+        command = 'shell input keyevent ' + key
+        print('按键：' + command)
+        print(self._adb.run(command))
--- main.py
+++ main.py
@@ -1,41 +1,10 @@
 import random
-import sys
-import time
 
 import xmltodict
-from common import common
-
-def adb_click(x, y):
-    command = 'shell input tap ' + str(x) + ' ' + str(y)
-    print('点击：' + command)
-    print(adb.run(command))
-    time.sleep(random.uniform(1, 2))
+from common.auto_adb import adb_tools
 
 
-def adb_swipe(x1, y1, x2 ,y2):
-    command = 'shell input swipe ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2)
-    print('滑动：' + command)
-    print(adb.run(command))
-    time.sleep(random.uniform(1, 2))
 
-
-def adb_input(text):
-    command = 'shell input text ' + text
-    print('输入：' + command)
-    print(adb.run(command))
-    time.sleep(random.uniform(1, 5))
-
-
-def adb_dump():
-    print('获取布局')
-    print(adb.run('shell uiautomator dump'))
-    print(adb.run('pull /sdcard/window_dump.xml'))
-
-
-def adb_key(key):
-    command = 'shell input keyevent ' + key
-    print('按键：' + command)
-    print(adb.run(command))
 
 
 def analysis(obj):
@@ -48,13 +17,13 @@ def analysis(obj):
             y2 = float(obj['@bounds'].split('][')[1][:-1].split(',')[1])
 
             print('添加为熟朋友')
-            adb_click(int(random.uniform(x1, x2)), int(random.uniform(y1, y2)))
+            adb_tools.adb_click(int(random.uniform(x1, x2)), int(random.uniform(y1, y2)))
 
             print('确认')
-            adb_click(int(random.uniform(558, 966)), int(random.uniform(757, 889)))
+            adb_tools.adb_click(int(random.uniform(558, 966)), int(random.uniform(757, 889)))
 
             print('返回')
-            adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
+            adb_tools.adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
 
 
         if obj['@text'] == '添加到通讯录':
@@ -65,9 +34,9 @@ def analysis(obj):
             y2 = float(obj['@bounds'].split('][')[1][:-1].split(',')[1])
 
             print('添加到通讯录')
-            adb_click(int(random.uniform(x1, x2)), int(random.uniform(y1, y2)))
+            adb_tools.adb_click(int(random.uniform(x1, x2)), int(random.uniform(y1, y2)))
 
-            adb_dump()
+            adb_tools.adb_dump()
 
             with open('window_dump.xml', 'r', encoding='utf-8') as rf:
                 window_dump = rf.read()
@@ -76,24 +45,24 @@ def analysis(obj):
                     analysis(xmltodict.parse(window_dump)['hierarchy']['node'])
                 else:
                     print('返回')
-                    adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
-                    adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                    adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                    adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
 
         if obj.__contains__('@resource-id'):
             if obj['@resource-id'] == 'com.tencent.mm:id/dx6':
                 for item in range(len(obj['@text'])):
                     print('删除字符')
-                    adb_key(str(67))
+                    adb_tools.adb_key(str(67))
 
                 print('填写申请')
-                adb_input('hello')
+                adb_tools.adb_input('hello')
 
                 print('发送')
-                adb_click(int(random.uniform(855, 1035)), int(random.uniform(100, 190)))
+                adb_tools.adb_click(int(random.uniform(855, 1035)), int(random.uniform(100, 190)))
 
                 print('返回')
-                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
-                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
 
         if obj.__contains__('node'):
             analysis(obj['node'])
@@ -113,18 +82,18 @@ def add_wechar():
 
         # 定焦输入框
         print('定焦输入框')
-        adb_click(int(random.uniform(50, 1035)), int(random.uniform(280, 370)))
+        adb_tools.adb_click(int(random.uniform(50, 1035)), int(random.uniform(280, 370)))
 
         # 填写微信号
         print('填写微信号')
-        adb_input(phone)
+        adb_tools.adb_input(phone)
 
         # 搜索好友
         print('搜索好友')
-        adb_click(int(random.uniform(55, 190)), int(random.uniform(240, 380)))
+        adb_tools.adb_click(int(random.uniform(55, 190)), int(random.uniform(240, 380)))
 
         # 获取布局
-        adb_dump()
+        adb_tools.adb_dump()
 
         with open('window_dump.xml', 'r', encoding='utf-8') as rf:
             window_dump = rf.read()
@@ -133,35 +102,17 @@ def add_wechar():
                 analysis(node_obj)
             elif '发消息' in window_dump:
                 print('已经是好友了，返回上一页')
-                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
                 print('返回上一页')
-                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
+                adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
             elif '该用户不存在' in window_dump:
                 print('用户不存在，返回上一页')
-                adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
-
-
-if sys.version_info.major != 3:
-    print('请使用Python3')
-    exit(1)
-try:
-    from common.auto_adb import auto_adb
-except Exception as ex:
-    print(ex)
-    print('请将脚本放在项目根目录中运行')
-    print('请检查项目根目录中的 common 文件夹是否存在')
-    exit(1)
-adb = auto_adb()
-
-VERSION = "0.0.1"
-
-adb.test_device()
-
+                adb_tools.adb_click(int(random.uniform(26, 98)), int(random.uniform(68, 212)))
 
 def add_ltb():
-    adb_swipe(30, 749, 30, 417)
+    adb_tools.adb_swipe(30, 749, 30, 417)
     # 获取布局
-    adb_dump()
+    adb_tools.adb_dump()
     with open('window_dump.xml', 'r', encoding='utf-8') as rf:
         window_dump = rf.read()
         if '添加为熟朋友' in window_dump:
@@ -169,7 +120,7 @@ def add_ltb():
             analysis(node_obj)
         elif '发送消息' in window_dump:
             print('已经是好友了，返回上一页')
-            adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
+            adb_tools.adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
 
 
 def add_ltb_main():
@@ -178,55 +129,66 @@ def add_ltb_main():
     print(int(count))
 
     for item in range(int(count)):
-        adb_click(int(random.uniform(30, 234)), int(random.uniform(417, 649)))
+        adb_tools.adb_click(int(random.uniform(30, 234)), int(random.uniform(417, 649)))
 
         add_ltb()
 
-        adb_click(int(random.uniform(234, 438)), int(random.uniform(417, 649)))
+        adb_tools.adb_click(int(random.uniform(234, 438)), int(random.uniform(417, 649)))
 
         add_ltb()
 
-        adb_click(int(random.uniform(438, 642)), int(random.uniform(417, 649)))
+        adb_tools.adb_click(int(random.uniform(438, 642)), int(random.uniform(417, 649)))
 
         add_ltb()
 
-        adb_click(int(random.uniform(642, 846)), int(random.uniform(417, 649)))
+        adb_tools.adb_click(int(random.uniform(642, 846)), int(random.uniform(417, 649)))
 
         add_ltb()
 
-        adb_click(int(random.uniform(846, 1050)), int(random.uniform(417, 649)))
+        adb_tools.adb_click(int(random.uniform(846, 1050)), int(random.uniform(417, 649)))
 
         add_ltb()
 
-        adb_swipe(30, 649, 30, 417)
+        adb_tools.adb_swipe(30, 649, 30, 417)
 
 
 def news():
     while 1:
 
         # adb_swipe(24, 375, 24, 1047)
-        adb_swipe(24, 1047, 24, 175)
+        adb_tools.adb_swipe(24, 1047, 24, 175)
 
-        adb_click(int(random.uniform(24, 1056)), int(random.uniform(375, 475)))# 1047
+        adb_tools.adb_click(int(random.uniform(24, 1056)), int(random.uniform(375, 475)))# 1047
 
-        adb_swipe(24, 1047, 24, 175)
-        adb_swipe(24, 1047, 24, 175)
-        adb_swipe(24, 1047, 24, 175)
-        adb_swipe(24, 1047, 24, 175)
-        adb_swipe(24, 1047, 24, 175)
+        adb_tools.adb_swipe(24, 1047, 24, 175)
+        adb_tools.adb_swipe(24, 1047, 24, 175)
+        adb_tools.adb_swipe(24, 1047, 24, 175)
+        adb_tools.adb_swipe(24, 1047, 24, 175)
+        adb_tools.adb_swipe(24, 1047, 24, 175)
 
-        adb_dump()
+        adb_tools.adb_dump()
         with open('window_dump.xml', 'r', encoding='utf-8') as rf:
             window_dump = rf.read()
             if 'com.bullet.messenger:id/news_share_outside_btn' in window_dump:
-                adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
+                adb_tools.adb_click(int(random.uniform(0, 150)), int(random.uniform(72, 216)))
 
 
 def while_send():
     while 1:
-        adb_click(int(random.uniform(838, 1030)), int(random.uniform(1155, 1347)))
+        adb_tools.adb_click(int(random.uniform(838, 1030)), int(random.uniform(1155, 1347)))
+
+
+def activation_hei_yu():
+    command = '-d shell sh /data/data/me.piebridge.brevent/brevent.sh'
+    print('激活：' + command)
+    print(adb_tools.adb_run(command))
+
 
 if __name__ == '__main__':
     # add_ltb_main()
     # while_send()
-    common.activation_hei_yu(adb)
+
+    adb_tools = adb_tools()
+    adb_tools.print()
+
+    activation_hei_yu()
